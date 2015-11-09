a185750d1d9944cf682536e412e5599a28f527d8
kazdragon <matthew.chaplain@gmail.com>
2015-11-09 08:02:25 +0000

    Refactored terminal read test.

--- test/terminal_read_test.cpp
+++ test/terminal_read_test.cpp
@@ -82,6 +82,21 @@ private :
 
 CPPUNIT_TEST_SUITE_REGISTRATION(terminal_read_test);
 
+template <class Expected> void expect_token(
+    std::string const &input,
+    Expected const &expected)
+{
+    terminalpp::terminal terminal;
+
+    auto result = terminal.read(input);
+
+    CPPUNIT_ASSERT_EQUAL(false, result.empty());
+
+    CPPUNIT_ASSERT_EQUAL(
+        boost::get<Expected>(expected),
+        boost::get<Expected>(result[0]));
+}
+
 void terminal_read_test::read_empty_string_yields_nothing()
 {
     terminalpp::terminal terminal;
@@ -95,139 +110,73 @@ void terminal_read_test::read_empty_string_yields_nothing()
 
 void terminal_read_test::read_character_yields_virtual_key()
 {
-    terminalpp::terminal terminal;
-
-    std::string input = "z";
-
-    auto expected = std::vector<terminalpp::token>{
-        terminalpp::virtual_key {
+    expect_token(
+        "z",
+        terminalpp::virtual_key{
             terminalpp::VK_LOWERCASE_Z,
             0,
             1,
-            { input[0] }
-        }
-    };
-
-    auto result = terminal.read(input);
-
-    CPPUNIT_ASSERT_EQUAL(expected.size(), result.size());
-    CPPUNIT_ASSERT_EQUAL(
-        boost::get<terminalpp::virtual_key>(expected[0]),
-        boost::get<terminalpp::virtual_key>(result[0]));
+            { 'z' }
+        });
 }
 
 void terminal_read_test::read_command_yields_command()
 {
-    terminalpp::terminal terminal;
-
-    std::string input = "\x1B[H";
-
-    auto expected = std::vector<terminalpp::token>{
+    expect_token(
+        "\x1B[H",
         terminalpp::ansi::control_sequence {
             '[',
             'H',
             false,
             { "" }
-        }
-    };
-
-    auto result = terminal.read(input);
-
-    CPPUNIT_ASSERT_EQUAL(expected.size(), result.size());
-    CPPUNIT_ASSERT_EQUAL(
-        boost::get<terminalpp::ansi::control_sequence>(expected[0]),
-        boost::get<terminalpp::ansi::control_sequence>(result[0]));
+        });
 }
 
 void terminal_read_test::read_command_with_arguments_yields_command_with_arguments()
 {
-    terminalpp::terminal terminal;
-
-    std::string input = "\x1B[22;33H";
-
-    auto expected = std::vector<terminalpp::token> {
+    expect_token(
+        "\x1B[22;33H",
         terminalpp::ansi::control_sequence {
             '[',
             'H',
             false,
             { "22", "33" }
-        }
-    };
-
-    auto result = terminal.read(input);
-
-    CPPUNIT_ASSERT_EQUAL(expected.size(), result.size());
-    CPPUNIT_ASSERT_EQUAL(
-        boost::get<terminalpp::ansi::control_sequence>(expected[0]),
-        boost::get<terminalpp::ansi::control_sequence>(result[0]));
+        });
 }
 
 void terminal_read_test::read_meta_command_yields_meta_command()
 {
-    terminalpp::terminal terminal;
-
-    std::string input = "\x1B\x1B[S";
-
-    auto expected = std::vector<terminalpp::token> {
+    expect_token(
+        "\x1B\x1B[S",
         terminalpp::ansi::control_sequence {
             '[',
             'S',
             true,
             { "" }
-        }
-    };
-
-    auto result = terminal.read(input);
-
-    CPPUNIT_ASSERT_EQUAL(expected.size(), result.size());
-    CPPUNIT_ASSERT_EQUAL(
-        boost::get<terminalpp::ansi::control_sequence>(expected[0]),
-        boost::get<terminalpp::ansi::control_sequence>(result[0]));
+        });
 }
 
 void terminal_read_test::read_mouse_command_yields_mouse_report()
 {
-    terminalpp::terminal terminal;
-
-    std::string input = "\x1B[M @B";
-
-    auto expected = std::vector<terminalpp::token> {
+    expect_token(
+        "\x1B[M @B",
         terminalpp::ansi::mouse::report {
             terminalpp::ansi::mouse::report::LEFT_BUTTON_DOWN,
             32,
             34
-        }
-    };
-
-    auto result = terminal.read(input);
-
-    CPPUNIT_ASSERT_EQUAL(expected.size(), result.size());
-    CPPUNIT_ASSERT_EQUAL(
-        boost::get<terminalpp::ansi::mouse::report>(expected[0]),
-        boost::get<terminalpp::ansi::mouse::report>(result[0]));
+        });
 }
 
 void terminal_read_test::read_non_mouse_similar_command_yields_command()
 {
-    terminalpp::terminal terminal;
-
-    std::string input = "\x1B?M";
-
-    auto expected = std::vector<terminalpp::token> {
+    expect_token(
+        "\x1B?M",
         terminalpp::ansi::control_sequence {
             '?',
             'M',
             false,
             { "" }
-        }
-    };
-
-    auto result = terminal.read(input);
-
-    CPPUNIT_ASSERT_EQUAL(expected.size(), result.size());
-    CPPUNIT_ASSERT_EQUAL(
-        boost::get<terminalpp::ansi::control_sequence>(expected[0]),
-        boost::get<terminalpp::ansi::control_sequence>(result[0]));
+        });
 }
 
 void terminal_read_test::read_partial_command_yields_nothing()
@@ -279,392 +228,205 @@ void terminal_read_test::read_partial_mouse_command_yields_nothing()
 
 void terminal_read_test::read_8bit_command_yields_command()
 {
-    terminalpp::terminal terminal;
-
-    std::string input = "\x9B""22;33H";
-
-    auto expected = std::vector<terminalpp::token> {
+    expect_token(
+        "\x9B""22;33H",
         terminalpp::ansi::control_sequence {
             '[',
             'H',
             false,
             { "22", "33" }
-        }
-    };
-
-    auto result = terminal.read(input);
-
-    CPPUNIT_ASSERT_EQUAL(expected.size(), result.size());
-    CPPUNIT_ASSERT_EQUAL(
-        boost::get<terminalpp::ansi::control_sequence>(expected[0]),
-        boost::get<terminalpp::ansi::control_sequence>(result[0]));
+        });
 }
 
 void terminal_read_test::cursor_up_command_yields_vk_up()
 {
-    terminalpp::terminal terminal;
-
-    std::string input = "\x1B[A";
-
-    auto expected = std::vector<terminalpp::token> {
-      terminalpp::virtual_key {
-          terminalpp::VK_UP,
-          0,
-          1,
-          terminalpp::ansi::control_sequence{'[', 'A', false, { "" }}
-      }
-    };
-
-    auto result = terminal.read(input);
-
-    CPPUNIT_ASSERT_EQUAL(expected.size(), result.size());
-    CPPUNIT_ASSERT_EQUAL(
-        boost::get<terminalpp::virtual_key>(expected[0]),
-        boost::get<terminalpp::virtual_key>(result[0]));
+    expect_token(
+        "\x1B[A",
+        terminalpp::virtual_key {
+            terminalpp::VK_UP,
+            0,
+            1,
+            terminalpp::ansi::control_sequence{'[', 'A', false, { "" }}
+        });
 }
 
 void terminal_read_test::cursor_down_command_yields_vk_down()
 {
-    terminalpp::terminal terminal;
-
-    std::string input = "\x1B[B";
-
-    auto expected = std::vector<terminalpp::token> {
-      terminalpp::virtual_key {
-          terminalpp::VK_DOWN,
-          0,
-          1,
-          terminalpp::ansi::control_sequence{'[', 'B', false, { "" }}
-      }
-    };
-
-    auto result = terminal.read(input);
-
-    CPPUNIT_ASSERT_EQUAL(expected.size(), result.size());
-    CPPUNIT_ASSERT_EQUAL(
-        boost::get<terminalpp::virtual_key>(expected[0]),
-        boost::get<terminalpp::virtual_key>(result[0]));
-}
-
-void terminal_read_test::cursor_left_command_yields_vk_left()
-{
-    terminalpp::terminal terminal;
-
-    std::string input = "\x1B[D";
-
-    auto expected = std::vector<terminalpp::token> {
-      terminalpp::virtual_key {
-          terminalpp::VK_LEFT,
-          0,
-          1,
-          terminalpp::ansi::control_sequence{'[', 'D', false, { "" }}
-      }
-    };
-
-    auto result = terminal.read(input);
-
-    CPPUNIT_ASSERT_EQUAL(expected.size(), result.size());
-    CPPUNIT_ASSERT_EQUAL(
-        boost::get<terminalpp::virtual_key>(expected[0]),
-        boost::get<terminalpp::virtual_key>(result[0]));
+    expect_token(
+        "\x1B[B",
+        terminalpp::virtual_key {
+            terminalpp::VK_DOWN,
+            0,
+            1,
+            terminalpp::ansi::control_sequence{'[', 'B', false, { "" }}
+        });
 }
 
 void terminal_read_test::cursor_right_command_yields_vk_right()
 {
-    terminalpp::terminal terminal;
+    expect_token(
+        "\x1B[C",
+        terminalpp::virtual_key {
+            terminalpp::VK_RIGHT,
+            0,
+            1,
+            terminalpp::ansi::control_sequence{'[', 'C', false, { "" }}
+        });
+}
 
-    std::string input = "\x1B[C";
-
-    auto expected = std::vector<terminalpp::token> {
-      terminalpp::virtual_key {
-          terminalpp::VK_RIGHT,
-          0,
-          1,
-          terminalpp::ansi::control_sequence{'[', 'C', false, { "" }}
-      }
-    };
-
-    auto result = terminal.read(input);
-
-    CPPUNIT_ASSERT_EQUAL(expected.size(), result.size());
-    CPPUNIT_ASSERT_EQUAL(
-        boost::get<terminalpp::virtual_key>(expected[0]),
-        boost::get<terminalpp::virtual_key>(result[0]));
+void terminal_read_test::cursor_left_command_yields_vk_left()
+{
+    expect_token(
+        "\x1B[D",
+        terminalpp::virtual_key {
+            terminalpp::VK_LEFT,
+            0,
+            1,
+            terminalpp::ansi::control_sequence{'[', 'D', false, { "" }}
+        });
 }
 
 void terminal_read_test::cursor_home_command_yields_vk_home()
 {
-    terminalpp::terminal terminal;
-
-    std::string input = "\x1B[1~";
-
-    auto expected = std::vector<terminalpp::token> {
-      terminalpp::virtual_key {
-          terminalpp::VK_HOME,
-          0,
-          1,
-          terminalpp::ansi::control_sequence{'[', '~', false, { "1" }}
-      }
-    };
-
-    auto result = terminal.read(input);
-
-    CPPUNIT_ASSERT_EQUAL(expected.size(), result.size());
-    CPPUNIT_ASSERT_EQUAL(
-        boost::get<terminalpp::virtual_key>(expected[0]),
-        boost::get<terminalpp::virtual_key>(result[0]));
+    expect_token(
+        "\x1B[1~",
+        terminalpp::virtual_key {
+            terminalpp::VK_HOME,
+            0,
+            1,
+            terminalpp::ansi::control_sequence{'[', '~', false, { "1" }}
+        });
 }
 
 void terminal_read_test::cursor_ins_command_yields_vk_ins()
 {
-    terminalpp::terminal terminal;
-
-    std::string input = "\x1B[2~";
-
-    auto expected = std::vector<terminalpp::token> {
-      terminalpp::virtual_key {
-          terminalpp::VK_INS,
-          0,
-          1,
-          terminalpp::ansi::control_sequence{'[', '~', false, { "2" }}
-      }
-    };
-
-    auto result = terminal.read(input);
-
-    CPPUNIT_ASSERT_EQUAL(expected.size(), result.size());
-    CPPUNIT_ASSERT_EQUAL(
-        boost::get<terminalpp::virtual_key>(expected[0]),
-        boost::get<terminalpp::virtual_key>(result[0]));
+    expect_token(
+        "\x1B[2~",
+        terminalpp::virtual_key {
+            terminalpp::VK_INS,
+            0,
+            1,
+            terminalpp::ansi::control_sequence{'[', '~', false, { "2" }}
+        });
 }
 
 void terminal_read_test::cursor_del_command_yields_vk_del()
 {
-    terminalpp::terminal terminal;
-
-    std::string input = "\x1B[3~";
-
-    auto expected = std::vector<terminalpp::token> {
-      terminalpp::virtual_key {
-          terminalpp::VK_DEL,
-          0,
-          1,
-          terminalpp::ansi::control_sequence{'[', '~', false, { "3" }}
-      }
-    };
-
-    auto result = terminal.read(input);
-
-    CPPUNIT_ASSERT_EQUAL(expected.size(), result.size());
-    CPPUNIT_ASSERT_EQUAL(
-        boost::get<terminalpp::virtual_key>(expected[0]),
-        boost::get<terminalpp::virtual_key>(result[0]));
+    expect_token(
+        "\x1B[3~",
+        terminalpp::virtual_key {
+            terminalpp::VK_DEL,
+            0,
+            1,
+            terminalpp::ansi::control_sequence{'[', '~', false, { "3" }}
+        });
 }
 
 void terminal_read_test::cursor_end_command_yields_vk_end()
 {
-    terminalpp::terminal terminal;
-
-    std::string input = "\x1B[4~";
-
-    auto expected = std::vector<terminalpp::token> {
-      terminalpp::virtual_key {
-          terminalpp::VK_END,
-          0,
-          1,
-          terminalpp::ansi::control_sequence{'[', '~', false, { "4" }}
-      }
-    };
-
-    auto result = terminal.read(input);
-
-    CPPUNIT_ASSERT_EQUAL(expected.size(), result.size());
-    CPPUNIT_ASSERT_EQUAL(
-        boost::get<terminalpp::virtual_key>(expected[0]),
-        boost::get<terminalpp::virtual_key>(result[0]));
+    expect_token(
+        "\x1B[4~",
+        terminalpp::virtual_key {
+            terminalpp::VK_END,
+            0,
+            1,
+            terminalpp::ansi::control_sequence{'[', '~', false, { "4" }}
+        });
 }
 
 void terminal_read_test::cursor_pgup_command_yields_vk_pgup()
 {
-    terminalpp::terminal terminal;
-
-    std::string input = "\x1B[5~";
-
-    auto expected = std::vector<terminalpp::token> {
-      terminalpp::virtual_key {
-          terminalpp::VK_PGUP,
-          0,
-          1,
-          terminalpp::ansi::control_sequence{'[', '~', false, { "5" }}
-      }
-    };
-
-    auto result = terminal.read(input);
-
-    CPPUNIT_ASSERT_EQUAL(expected.size(), result.size());
-    CPPUNIT_ASSERT_EQUAL(
-        boost::get<terminalpp::virtual_key>(expected[0]),
-        boost::get<terminalpp::virtual_key>(result[0]));
+    expect_token(
+        "\x1B[5~",
+        terminalpp::virtual_key {
+            terminalpp::VK_PGUP,
+            0,
+            1,
+            terminalpp::ansi::control_sequence{'[', '~', false, { "5" }}
+        });
 }
 
 void terminal_read_test::cursor_pgdn_command_yields_vk_pgdn()
 {
-    terminalpp::terminal terminal;
-
-    std::string input = "\x1B[6~";
-
-    auto expected = std::vector<terminalpp::token> {
-      terminalpp::virtual_key {
-          terminalpp::VK_PGDN,
-          0,
-          1,
-          terminalpp::ansi::control_sequence{'[', '~', false, { "6" }}
-      }
-    };
-
-    auto result = terminal.read(input);
-
-    CPPUNIT_ASSERT_EQUAL(expected.size(), result.size());
-    CPPUNIT_ASSERT_EQUAL(
-        boost::get<terminalpp::virtual_key>(expected[0]),
-        boost::get<terminalpp::virtual_key>(result[0]));
+    expect_token(
+        "\x1B[6~",
+        terminalpp::virtual_key {
+            terminalpp::VK_PGDN,
+            0,
+            1,
+            terminalpp::ansi::control_sequence{'[', '~', false, { "6" }}
+        });
 }
 
 void terminal_read_test::cursor_up_ss3_yields_vk_up()
 {
-    terminalpp::terminal terminal;
-
-    std::string input = "\x1BOA";
-
-    auto expected = std::vector<terminalpp::token> {
-      terminalpp::virtual_key {
-          terminalpp::VK_UP,
-          0,
-          1,
-          terminalpp::ansi::control_sequence{'O', 'A', false, { "" }}
-      }
-    };
-
-    auto result = terminal.read(input);
-
-    CPPUNIT_ASSERT_EQUAL(expected.size(), result.size());
-    CPPUNIT_ASSERT_EQUAL(
-        boost::get<terminalpp::virtual_key>(expected[0]),
-        boost::get<terminalpp::virtual_key>(result[0]));
+    expect_token(
+        "\x1BOA",
+        terminalpp::virtual_key {
+            terminalpp::VK_UP,
+            0,
+            1,
+            terminalpp::ansi::control_sequence{'O', 'A', false, { "" }}
+        });
 }
 
 void terminal_read_test::cursor_down_ss3_yields_vk_down()
 {
-    terminalpp::terminal terminal;
-
-    std::string input = "\x1BOB";
-
-    auto expected = std::vector<terminalpp::token> {
-      terminalpp::virtual_key {
-          terminalpp::VK_DOWN,
-          0,
-          1,
-          terminalpp::ansi::control_sequence{'O', 'B', false, { "" }}
-      }
-    };
-
-    auto result = terminal.read(input);
-
-    CPPUNIT_ASSERT_EQUAL(expected.size(), result.size());
-    CPPUNIT_ASSERT_EQUAL(
-        boost::get<terminalpp::virtual_key>(expected[0]),
-        boost::get<terminalpp::virtual_key>(result[0]));
+    expect_token(
+        "\x1BOB",
+        terminalpp::virtual_key {
+            terminalpp::VK_DOWN,
+            0,
+            1,
+            terminalpp::ansi::control_sequence{'O', 'B', false, { "" }}
+        });
 }
 
 void terminal_read_test::cursor_right_ss3_yields_vk_right()
 {
-    terminalpp::terminal terminal;
-
-    std::string input = "\x1BOC";
-
-    auto expected = std::vector<terminalpp::token> {
-      terminalpp::virtual_key {
-          terminalpp::VK_RIGHT,
-          0,
-          1,
-          terminalpp::ansi::control_sequence{'O', 'C', false, { "" }}
-      }
-    };
-
-    auto result = terminal.read(input);
-
-    CPPUNIT_ASSERT_EQUAL(expected.size(), result.size());
-    CPPUNIT_ASSERT_EQUAL(
-        boost::get<terminalpp::virtual_key>(expected[0]),
-        boost::get<terminalpp::virtual_key>(result[0]));
+    expect_token(
+        "\x1BOC",
+        terminalpp::virtual_key {
+            terminalpp::VK_RIGHT,
+            0,
+            1,
+            terminalpp::ansi::control_sequence{'O', 'C', false, { "" }}
+        });
 }
 
 void terminal_read_test::cursor_left_ss3_yields_vk_left()
 {
-    terminalpp::terminal terminal;
-
-    std::string input = "\x1BOD";
-
-    auto expected = std::vector<terminalpp::token> {
-      terminalpp::virtual_key {
-          terminalpp::VK_LEFT,
-          0,
-          1,
-          terminalpp::ansi::control_sequence{'O', 'D', false, { "" }}
-      }
-    };
-
-    auto result = terminal.read(input);
-
-    CPPUNIT_ASSERT_EQUAL(expected.size(), result.size());
-    CPPUNIT_ASSERT_EQUAL(
-        boost::get<terminalpp::virtual_key>(expected[0]),
-        boost::get<terminalpp::virtual_key>(result[0]));
+    expect_token(
+        "\x1BOD",
+        terminalpp::virtual_key {
+            terminalpp::VK_LEFT,
+            0,
+            1,
+            terminalpp::ansi::control_sequence{'O', 'D', false, { "" }}
+        });
 }
 
 void terminal_read_test::cursor_home_ss3_yields_vk_home()
 {
-    terminalpp::terminal terminal;
-
-    std::string input = "\x1BOH";
-
-    auto expected = std::vector<terminalpp::token> {
-      terminalpp::virtual_key {
-          terminalpp::VK_HOME,
-          0,
-          1,
-          terminalpp::ansi::control_sequence{'O', 'H', false, { "" }}
-      }
-    };
-
-    auto result = terminal.read(input);
-
-    CPPUNIT_ASSERT_EQUAL(expected.size(), result.size());
-    CPPUNIT_ASSERT_EQUAL(
-        boost::get<terminalpp::virtual_key>(expected[0]),
-        boost::get<terminalpp::virtual_key>(result[0]));
+    expect_token(
+        "\x1BOH",
+        terminalpp::virtual_key {
+            terminalpp::VK_HOME,
+            0,
+            1,
+            terminalpp::ansi::control_sequence{'O', 'H', false, { "" }}
+        });
 }
 
 void terminal_read_test::cursor_end_ss3_yields_vk_end()
 {
-    terminalpp::terminal terminal;
-
-    std::string input = "\x1BOF";
-
-    auto expected = std::vector<terminalpp::token> {
-      terminalpp::virtual_key {
-          terminalpp::VK_END,
-          0,
-          1,
-          terminalpp::ansi::control_sequence{'O', 'F', false, { "" }}
-      }
-    };
-
-    auto result = terminal.read(input);
-
-    CPPUNIT_ASSERT_EQUAL(expected.size(), result.size());
-    CPPUNIT_ASSERT_EQUAL(
-        boost::get<terminalpp::virtual_key>(expected[0]),
-        boost::get<terminalpp::virtual_key>(result[0]));
+    expect_token(
+        "\x1BOF",
+        terminalpp::virtual_key {
+            terminalpp::VK_END,
+            0,
+            1,
+            terminalpp::ansi::control_sequence{'O', 'F', false, { "" }}
+        });
 }
 
