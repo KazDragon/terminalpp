8dd4b38bf710440541c1813959edba2fd1f9f298
kazdragon <matthew.chaplain@gmail.com>
2016-01-04 16:20:54 +0000

    Added encoder.hpp to convenience header.
    Closes #78.

--- include/terminalpp/terminalpp.hpp
+++ include/terminalpp/terminalpp.hpp
@@ -3,13 +3,16 @@
 
 // The basic layer of terminal reading and writing is defined in terminal.hpp,
 // which includes everything you need to know about the terminal, including
-// token types (virtual keys, control sequences, mouse reports), 
-// manipulation of elements to form strings, and basic cursor and size 
+// token types (virtual keys, control sequences, mouse reports),
+// manipulation of elements to form strings, and basic cursor and size
 // manipulation.
 #include "terminalpp/terminal.hpp"
 
 // More advanced knowledge of the terminal comes in the form of the screen,
 // which embeds canvases to be drawn on.
 #include "terminalpp/screen.hpp"
+
+// In order to write encoded strings, it is necessary to include the encoder.
+#include "terminalpp/encoder.hpp"
 
 #endif
