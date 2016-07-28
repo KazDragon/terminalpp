68dd9f0ce1e441600bf17cb5a4a105ebcc2ebc60
kazdragon <matthew.chaplain@gmail.com>
2016-07-28 05:55:28 +0000

    Completed debug output of attributes.

--- src/attribute.cpp
+++ src/attribute.cpp
@@ -3,6 +3,9 @@
 
 namespace terminalpp {
 
+static const auto default_colour =
+    low_colour(terminalpp::ansi::graphics::colour::default_);
+
 // ==========================================================================
 // STREAM OPERATOR
 // ==========================================================================
@@ -111,13 +114,163 @@ std::ostream &operator<<(std::ostream &out, colour const &col)
 }
 
 // ==========================================================================
+// APPEND_COMMA_IF_NECESSARY
+// ==========================================================================
+static void append_comma_if_necessary(std::ostream &out, bool necessary)
+{
+    if (necessary)
+    {
+        out << ",";
+    }
+}
+
+// ==========================================================================
+// APPEND_FOREGROUND_COLOUR
+// ==========================================================================
+static void append_foreground_colour(
+    std::ostream &out,
+    attribute const &attr,
+    bool &comma)
+{
+    if (attr.foreground_colour_ != default_colour)
+    {
+        append_comma_if_necessary(out, comma);
+        out << "fg:" << attr.foreground_colour_;
+        comma = true;
+    }
+}
+
+// ==========================================================================
+// APPEND_BACKGROUND_COLOUR
+// ==========================================================================
+static void append_background_colour(
+    std::ostream &out,
+    attribute const &attr,
+    bool &comma)
+{
+    if (attr.background_colour_ != default_colour)
+    {
+        append_comma_if_necessary(out, comma);
+        out << "fg:" << attr.background_colour_;
+        comma = true;
+    }
+}
+
+// ==========================================================================
+// APPEND_INTENSITY
+// ==========================================================================
+static void append_intensity(
+    std::ostream &out,
+    attribute const &attr,
+    bool &comma)
+{
+    static const auto default_intensity =
+        terminalpp::ansi::graphics::intensity::normal;
+
+    if (attr.intensity_ != default_intensity)
+    {
+        append_comma_if_necessary(out, comma);
+
+        if (attr.intensity_ == ansi::graphics::intensity::bold)
+        {
+            out << "bold";
+        }
+        else if (attr.intensity_ == ansi::graphics::intensity::faint)
+        {
+            out << "faint";
+        }
+
+        comma = true;
+    }
+}
+
+// ==========================================================================
+// APPEND_UNDERLINING
+// ==========================================================================
+static void append_underlining(
+    std::ostream &out,
+    attribute const &attr,
+    bool &comma)
+{
+    static const auto default_underlining =
+        terminalpp::ansi::graphics::underlining::not_underlined;
+
+    if (attr.underlining_ != default_underlining)
+    {
+        append_comma_if_necessary(out, comma);
+
+        if (attr.underlining_ == ansi::graphics::underlining::underlined)
+        {
+            out << "underlined";
+        }
+
+        comma = true;
+    }
+}
+
+// ==========================================================================
+// APPEND_POLARITY
+// ==========================================================================
+static void append_polarity(
+    std::ostream &out,
+    attribute const &attr,
+    bool &comma)
+{
+    static const auto default_polarity =
+        terminalpp::ansi::graphics::polarity::positive;
+
+    if (attr.polarity_ != default_polarity)
+    {
+        append_comma_if_necessary(out, comma);
+
+        if (attr.polarity_ == ansi::graphics::polarity::negative)
+        {
+            out << "inverse";
+        }
+
+        comma = true;
+    }
+}
+
+// ==========================================================================
+// APPEND_BLINKING
+// ==========================================================================
+static void append_blinking(
+    std::ostream &out,
+    attribute const &attr,
+    bool &comma)
+{
+    static const auto default_blinking =
+        terminalpp::ansi::graphics::blinking::steady;
+
+    if (attr.blinking_ != default_blinking)
+    {
+        append_comma_if_necessary(out, comma);
+
+        if (attr.blinking_ == ansi::graphics::blinking::blink)
+        {
+            out << "blinking";
+        }
+
+        comma = true;
+    }
+}
+
+// ==========================================================================
 // STREAM OPERATOR
 // ==========================================================================
 std::ostream &operator<<(std::ostream &out, attribute const &attr)
 {
+    bool comma = false;
+
     out << "attr[";
 
-    // TODO: add attributes.
+    append_foreground_colour(out, attr, comma);
+    append_background_colour(out, attr, comma);
+    append_intensity(out, attr, comma);
+    append_underlining(out, attr, comma);
+    append_polarity(out, attr, comma);
+    append_blinking(out, attr, comma);
 
     out << "]";
 
