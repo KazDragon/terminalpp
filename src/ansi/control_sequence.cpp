d1416ff5305a20a98e33a4cd7ccef790454dd900
Matthew Chaplain <matthew.chaplain@gmail.com>
2017-06-21 08:31:07 +0200

    Simplified ansi control sequence equality operator.

--- src/ansi/control_sequence.cpp
+++ src/ansi/control_sequence.cpp
@@ -1,34 +1,14 @@
 #include "terminalpp/ansi/control_sequence.hpp"
-#include <iostream>
 
 namespace terminalpp { namespace ansi {
 
 bool operator==(control_sequence const &lhs, control_sequence const &rhs)
 {
-    if (lhs.initiator == rhs.initiator
+    return lhs.initiator == rhs.initiator
         && lhs.command == rhs.command
-        && lhs.meta == rhs.meta)
-    {
-        // TODO: look up an algorithm to do this.  Mismatch? Equal_Range
-        if (lhs.arguments.size() != rhs.arguments.size())
-        {
-            return false;
-        }
-
-        for (auto itl = lhs.arguments.begin(), itr = rhs.arguments.begin();
-             itl != lhs.arguments.end();
-             ++itl, ++itr)
-        {
-            if (*itl != *itr)
-            {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    return false;
+        && lhs.meta == rhs.meta
+        && lhs.arguments.size() == rhs.arguments.size()
+        && lhs.arguments == rhs.arguments;
 }
 
 }}
