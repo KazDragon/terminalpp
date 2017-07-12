8c974be3abbeac39814290a6920039be8deb685f
KazDragon <matthew.chaplain@gmail.com>
2017-07-12 10:27:00 +0200

    Added tests for disabling mouse and switching screen buffers.

--- test/default_terminal_settings_test.cpp
+++ test/default_terminal_settings_test.cpp
@@ -12,7 +12,16 @@ TEST(enabling_mouse_mode_with_default_behaviour, sends_nothing)
         terminal.enable_mouse());
 }
 
-TEST(enabling_mouse_mode_with_basic_mouse_support, sends_basic_mouse_tracking)
+TEST(disabling_mouse_mode_with_default_behaviour, sends_nothing)
+{
+    terminalpp::default_terminal terminal;
+
+    expect_sequence(
+        std::string(""),
+        terminal.disable_mouse());
+}
+
+TEST(enabling_mouse_mode_with_basic_mouse_support, sends_enable_basic_mouse_tracking)
 {
     terminalpp::behaviour behaviour;
     behaviour.supports_basic_mouse_tracking = true;
@@ -24,7 +33,19 @@ TEST(enabling_mouse_mode_with_basic_mouse_support, sends_basic_mouse_tracking)
         terminal.enable_mouse());
 }
 
-TEST(enabling_mouse_mode_with_all_mouse_tracking_support, sends_all_mouse_tracking)
+TEST(disabling_mouse_mode_with_basic_mouse_support, sends_disable_basic_mouse_tracking)
+{
+    terminalpp::behaviour behaviour;
+    behaviour.supports_basic_mouse_tracking = true;
+
+    terminalpp::default_terminal terminal(behaviour);
+
+    expect_sequence(
+        std::string("\x1B[?1000l"),
+        terminal.disable_mouse());
+}
+
+TEST(enabling_mouse_mode_with_all_mouse_tracking_support, sends_enable_all_mouse_tracking)
 {
     terminalpp::behaviour behaviour;
     behaviour.supports_all_mouse_motion_tracking = true;
@@ -33,6 +54,17 @@ TEST(enabling_mouse_mode_with_all_mouse_tracking_support, sends_all_mouse_tracki
     expect_sequence(
         std::string("\x1B[?1003h"),
         terminal.enable_mouse());
+}
+
+TEST(disabling_mouse_mode_with_all_mouse_tracking_support, sends_disable_all_mouse_tracking)
+{
+    terminalpp::behaviour behaviour;
+    behaviour.supports_all_mouse_motion_tracking = true;
+    terminalpp::default_terminal terminal(behaviour);
+
+    expect_sequence(
+        std::string("\x1B[?1003l"),
+        terminal.disable_mouse());
 }
 
 TEST(setting_window_title_with_default_behaviour, sends_nothing)
@@ -64,4 +96,22 @@ TEST(setting_window_title_with_st_behaviour, sends_window_title_with_st)
     expect_sequence(
         std::string("\x1B]2;title\x1B\\"),
         terminal.set_window_title("title"));
+}
+
+TEST(activating_normal_screen_buffer, sends_use_normal_screen_buffer)
+{
+    terminalpp::default_terminal terminal;
+
+    expect_sequence(
+        std::string("\x1B[?47l"),
+        terminal.use_normal_screen_buffer());
+}
+
+TEST(activating_alternate_screen_buffer, sends_use_alternate_screen_buffer)
+{
+    terminalpp::default_terminal terminal;
+
+    expect_sequence(
+        std::string("\x1B[?47h"),
+        terminal.use_alternate_screen_buffer());
 }
