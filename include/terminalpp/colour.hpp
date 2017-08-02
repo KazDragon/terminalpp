b7069ec55d3352e86d292ad5d4a2fa8eb0f0b289
kazdragon <matthew.chaplain@gmail.com>
2017-08-02 14:53:55 +0000

    Factored colours and effects out of attributes.

---
+++ include/terminalpp/colour.hpp
@@ -0,0 +1,255 @@
+#pragma once
+#include "terminalpp/ansi/protocol.hpp"
+#include <iosfwd>
+
+namespace terminalpp {
+
+//* =========================================================================
+/// \brief Structure representing a normal ANSI 16-colour value
+//* =========================================================================
+struct low_colour
+{
+    //* =====================================================================
+    /// \brief Constructs a low_colour with the "default" colour value.
+    //* =====================================================================
+    constexpr low_colour()
+        : low_colour(terminalpp::ansi::graphics::colour::default_)
+    {
+    }
+
+    //* =====================================================================
+    /// \brief Constructs a low_colour from the passed-in ANSI colour.
+    //* =====================================================================
+    constexpr low_colour(terminalpp::ansi::graphics::colour colour)
+        : value_(colour)
+    {
+    };
+
+    terminalpp::ansi::graphics::colour value_;
+};
+
+//* =========================================================================
+/// \brief Equality operator for low_colours.
+//* =========================================================================
+constexpr bool operator==(low_colour const &lhs, low_colour const &rhs)
+{
+    return lhs.value_ == rhs.value_;
+}
+
+//* =========================================================================
+/// \brief Inequality operator for low_colours.
+//* =========================================================================
+constexpr bool operator!=(low_colour const &lhs, low_colour const &rhs)
+{
+    return !(lhs == rhs);
+}
+
+//* =========================================================================
+/// \brief Streaming output operator for low_colours.  Prints the text
+/// equivalent of the colour (e.g. red, blue, black)
+//* =========================================================================
+TERMINALPP_EXPORT
+std::ostream &operator<<(std::ostream &out, low_colour const &col);
+
+//* =========================================================================
+/// \brief Structure representing the central 216 colours of a 256-colour
+/// palette, where each colour channel is represented by a number in the
+/// range 0..5.
+//* =========================================================================
+struct high_colour
+{
+    //* =====================================================================
+    /// \brief Default constructs a high-colour with the value of pure black.
+    //* =====================================================================
+    constexpr high_colour()
+      : high_colour(0, 0, 0)
+    {
+    }
+
+    //* =====================================================================
+    /// \brief Constructs a high_colour from the passed-in RGB values, each
+    /// of which should be in the range 0-5.
+    //* =====================================================================
+    constexpr high_colour(
+        byte red,
+        byte green,
+        byte blue)
+      : value_(red * 36 + green * 6 + blue + 16)
+    {
+    }
+
+    byte value_;
+};
+
+//* =========================================================================
+/// \brief Equality operator for high_colours.
+//* =========================================================================
+constexpr bool operator==(high_colour const &lhs, high_colour const &rhs)
+{
+    return lhs.value_ == rhs.value_;
+}
+
+//* =========================================================================
+/// \brief Inequality operator for high_colours.
+//* =========================================================================
+constexpr bool operator!=(high_colour const &lhs, high_colour const &rhs)
+{
+    return !(lhs == rhs);
+}
+
+//* =========================================================================
+/// \brief Streaming output operator for high_colours.  Prints the text
+/// equivalent of the colour (e.g. #234, #512, etc.)
+//* =========================================================================
+TERMINALPP_EXPORT
+std::ostream &operator<<(std::ostream &out, high_colour const &col);
+
+//* =========================================================================
+/// \brief Structure representing the 24 greyscale tones of a 256-colour
+/// palette
+//* =========================================================================
+struct greyscale_colour
+{
+    //* =====================================================================
+    /// \brief Default constructs a greyscale value with the darkest
+    /// available grey.
+    //* =====================================================================
+    constexpr greyscale_colour()
+      : shade_(0)
+    {
+    }
+
+    //* =====================================================================
+    /// \brief Constructs a greyscale value from the given shade, which
+    /// should be in the range 0-23.
+    //* =====================================================================
+    constexpr explicit greyscale_colour(byte shade)
+      : shade_(shade + 232)
+    {
+    }
+
+    byte shade_;
+};
+
+//* =========================================================================
+/// \brief Equality operator for greyscale_colours.
+//* =========================================================================
+constexpr bool operator==(
+    greyscale_colour const &lhs, greyscale_colour const &rhs)
+{
+    return lhs.shade_ == rhs.shade_;
+}
+
+//* =========================================================================
+/// \brief Inequality operator for greyscale_colours.
+//* =========================================================================
+constexpr bool operator!=(
+    greyscale_colour const &lhs, greyscale_colour const &rhs)
+{
+    return !(lhs == rhs);
+}
+
+//* =========================================================================
+/// \brief Streaming output operator for greyscale_colours.  Prints the text
+/// equivalent of the colour (e.g. #22, #00, etc.)
+//* =========================================================================
+TERMINALPP_EXPORT
+std::ostream &operator<<(std::ostream &out, greyscale_colour const &col);
+
+//* =========================================================================
+/// \brief Structure representing a sum type of the available colour styles.
+//* =========================================================================
+struct colour
+{
+    //* =====================================================================
+    /// \brief An enumeration of the possible colour types.
+    //* =====================================================================
+    enum class type : byte
+    {
+        low, high, greyscale
+    };
+
+    //* =====================================================================
+    /// \brief Default constructs the colour with the "default" ANSI colour.
+    //* =====================================================================
+    constexpr colour()
+      : colour(terminalpp::low_colour())
+    {
+    }
+
+    //* =====================================================================
+    /// \brief Constructs a colour with the passed low_colour value.
+    //* =====================================================================
+    constexpr colour(terminalpp::low_colour col)
+      : low_colour_(std::move(col)),
+        type_(type::low)
+    {
+    }
+
+    //* =====================================================================
+    /// \brief Constructs a colour with the passed high_colour value.
+    //* =====================================================================
+    constexpr colour(terminalpp::high_colour col)
+      : high_colour_(std::move(col)),
+        type_(type::high)
+    {
+    }
+
+    //* =====================================================================
+    /// \brief Constructs a colour with the passed greyscale_colour value.
+    //* =====================================================================
+    constexpr colour(terminalpp::greyscale_colour col)
+      : greyscale_colour_(std::move(col)),
+        type_(type::greyscale)
+    {
+    }
+
+    //* =====================================================================
+    /// \brief Copy constructor
+    //* =====================================================================
+    constexpr colour(terminalpp::ansi::graphics::colour col)
+      : colour(terminalpp::low_colour(col))
+    {
+    }
+
+    union
+    {
+        terminalpp::low_colour low_colour_;
+        terminalpp::high_colour high_colour_;
+        terminalpp::greyscale_colour greyscale_colour_;
+    };
+
+    type type_;
+};
+
+//* =========================================================================
+/// \brief Equality operator for colours.
+//* =========================================================================
+constexpr bool operator==(colour const &lhs, colour const &rhs)
+{
+    return lhs.type_             == rhs.type_
+        && lhs.type_             == colour::type::low
+         ? lhs.low_colour_       == rhs.low_colour_
+         : lhs.type_             == colour::type::high
+         ? lhs.high_colour_      == rhs.high_colour_
+         : lhs.type_             == colour::type::greyscale
+         ? lhs.greyscale_colour_ == rhs.greyscale_colour_
+         : false;
+}
+
+//* =========================================================================
+/// \brief Inequality operator for colours.
+//* =========================================================================
+constexpr bool operator!=(colour const &lhs, colour const &rhs)
+{
+    return !(lhs == rhs);
+}
+
+//* =========================================================================
+/// \brief Streaming output operator for colours.  Prints the text equivalent
+/// of the colour (e.g. "red", "#234", "#07", etc.)
+//* =========================================================================
+TERMINALPP_EXPORT
+std::ostream &operator<<(std::ostream &out, colour const &col);
+
+}
---
+++ test/colour_test.cpp
@@ -0,0 +1,171 @@
+#include "terminalpp/colour.hpp"
+#include <gtest/gtest.h>
+#include <tuple>
+
+using testing::ValuesIn;
+
+TEST(colour_test, can_construct_from_low_colour_enum)
+{
+    terminalpp::ansi::graphics::colour col(
+        terminalpp::ansi::graphics::colour::red);
+
+    terminalpp::colour expected_colour = terminalpp::low_colour(col);
+
+    auto result_colour = [](terminalpp::colour const &col)
+    {
+        return col;
+    }(col);
+
+    ASSERT_EQ(expected_colour, result_colour);
+}
+
+using low_colour_string = std::tuple<
+    terminalpp::low_colour,
+    std::string
+>;
+
+class low_colours_with_strings
+    : public testing::TestWithParam<low_colour_string>
+{
+};
+
+TEST_P(low_colours_with_strings, can_be_streamed_to_an_ostream)
+{
+    auto const &param = GetParam();
+    auto const &low_colour = std::get<0>(param);
+    auto const &expected_string = std::get<1>(param);
+
+    std::stringstream stream;
+    std::ostream &out = stream;
+
+    out << terminalpp::low_colour(low_colour);
+    ASSERT_EQ(expected_string, stream.str());
+}
+
+static low_colour_string const low_colour_strings[] = {
+  low_colour_string{ terminalpp::ansi::graphics::colour::black,    "black"   },
+  low_colour_string{ terminalpp::ansi::graphics::colour::red,      "red"     },
+  low_colour_string{ terminalpp::ansi::graphics::colour::green,    "green"   },
+  low_colour_string{ terminalpp::ansi::graphics::colour::yellow,   "yellow"  },
+  low_colour_string{ terminalpp::ansi::graphics::colour::blue,     "blue"    },
+  low_colour_string{ terminalpp::ansi::graphics::colour::magenta,  "magenta" },
+  low_colour_string{ terminalpp::ansi::graphics::colour::cyan,     "cyan"    },
+  low_colour_string{ terminalpp::ansi::graphics::colour::white,    "white"   },
+  low_colour_string{ terminalpp::ansi::graphics::colour::default_, "default" }
+};
+
+INSTANTIATE_TEST_CASE_P(
+    low_colours_can_be_streamed_to_an_ostream,
+    low_colours_with_strings,
+    ValuesIn(low_colour_strings)
+);
+
+using high_colour_string = std::tuple<
+    terminalpp::byte,    // red
+    terminalpp::byte,    // green
+    terminalpp::byte,    // blue
+    std::string          // expected output
+>;
+
+class high_colours_with_strings
+   : public testing::TestWithParam<high_colour_string>
+{
+};
+
+TEST_P(high_colours_with_strings, can_be_streamed_to_an_ostream)
+{
+    auto const &param = GetParam();
+    auto const &red   = std::get<0>(param);
+    auto const &green = std::get<1>(param);
+    auto const &blue  = std::get<2>(param);
+    auto const &expected_string = std::get<3>(param);
+
+    std::stringstream stream;
+    std::ostream &out = stream;
+
+    out << terminalpp::high_colour(red, green, blue);
+    ASSERT_EQ(expected_string, stream.str());
+}
+
+static high_colour_string const high_colour_strings[] = {
+    high_colour_string{ 0, 0, 0, "#000" },
+    high_colour_string{ 3, 0, 0, "#300" },
+    high_colour_string{ 0, 4, 0, "#040" },
+    high_colour_string{ 0, 0, 5, "#005" },
+    high_colour_string{ 5, 3, 1, "#531" }
+};
+
+INSTANTIATE_TEST_CASE_P(
+    high_colours_can_be_streamed_to_an_ostream,
+    high_colours_with_strings,
+    ValuesIn(high_colour_strings)
+);
+
+using greyscale_string = std::tuple<
+    terminalpp::byte,    // shade
+    std::string          // expected output
+>;
+
+class greyscale_colours_with_strings
+   : public testing::TestWithParam<greyscale_string>
+{
+};
+
+TEST_P(greyscale_colours_with_strings, can_be_streamed_to_an_ostream)
+{
+    auto const &param = GetParam();
+    auto const &shade = std::get<0>(param);
+    auto const &expected_string = std::get<1>(param);
+
+    std::stringstream stream;
+    std::ostream &out = stream;
+
+    out << terminalpp::greyscale_colour(shade);
+    ASSERT_EQ(expected_string, stream.str());
+}
+
+static greyscale_string const greyscale_strings[] = {
+    greyscale_string{ 0,  "#00" },
+    greyscale_string{ 9,  "#09" },
+    greyscale_string{ 10, "#10" },
+    greyscale_string{ 17, "#17" },
+    greyscale_string{ 22, "#22" },
+};
+
+INSTANTIATE_TEST_CASE_P(
+    greyscale_colours_can_be_streamed_to_an_ostream,
+    greyscale_colours_with_strings,
+    ValuesIn(greyscale_strings)
+);
+
+using colour_string = std::tuple<
+    terminalpp::colour,
+    std::string
+>;
+
+class colours_with_strings
+   : public testing::TestWithParam<colour_string>
+{
+};
+
+TEST_P(colours_with_strings, can_be_streamed_to_an_ostream)
+{
+    auto const &param = GetParam();
+    auto const &colour = std::get<0>(param);
+    auto const &expected_string = std::get<1>(param);
+
+    std::stringstream stream;
+    std::ostream &out = stream;
+
+    out << colour;
+    ASSERT_EQ(expected_string, stream.str());
+}
+
+static colour_string const colour_strings[] = {
+    colour_string{ terminalpp::ansi::graphics::colour::red,   "red"   },
+    colour_string{ terminalpp::ansi::graphics::colour::green, "green" },
+    colour_string{ terminalpp::high_colour(1, 2, 3),          "#123"  },
+    colour_string{ terminalpp::high_colour(5, 5, 4),          "#554"  },
+    colour_string{ terminalpp::greyscale_colour(0),           "#00"   },
+    colour_string{ terminalpp::greyscale_colour(21),          "#21"   },
+};
