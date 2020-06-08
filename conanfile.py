ae14b86aab3830ecec003a8e12ced48794645c05
KazDragon <matthew.chaplain@gmail.com>
2020-06-08 09:36:57 +0200

    Updated to version 1.3.5

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from conans import ConanFile, CMake, tools
 
 class TerminalppConan(ConanFile):
     name = "terminalpp"
-    version = "1.3.4"
+    version = "1.3.5"
     license = "MIT"
     author = "KazDragon"
     url = "https://github.com/KazDragon/terminalpp"
