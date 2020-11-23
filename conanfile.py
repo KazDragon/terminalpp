377465363f7c7bab3640062d10d5c49a7171997f
KazDragon <matthew.chaplain@gmail.com>
2020-11-23 08:30:10 +0100

    Terminal++ can now be built using system dependencies or Conan.

---
+++ conanfile.py
@@ -0,0 +1,42 @@
+from conans import ConanFile, CMake, tools
+
+
+class TerminalppConan(ConanFile):
+    name = "terminalpp"
+    version = "1.4.1"
+    license = "MIT"
+    author = "KazDragon"
+    url = "https://github.com/KazDragon/terminalpp"
+    description = "A C++ library for interacting with ANSI terminal windows"
+    topics = ("terminal-emulators", "ansi-escape-codes")
+    settings = "os", "compiler", "build_type", "arch"
+    exports = "*.hpp", "*.in", "*.cpp", "CMakeLists.txt", "*.md", "LICENSE"
+    options = {"shared": [True, False], "coverage": [True, False], "sanitize" : ["off", "address"]}
+    default_options = {"shared": False, "coverage": False, "sanitize": "off"}
+    requires = ("fmt/[>=5.3]",
+                "boost/[>=1.69]")
+    build_requires = ("gtest/[>=1.8.1]")
+    generators = "cmake"
+
+    def configure(self):
+        self.options["boost"].without_test = True
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.definitions["BUILD_SHARED_LIBS"] = self.options.shared
+        cmake.definitions["TERMINALPP_COVERAGE"] = self.options.coverage
+        cmake.definitions["TERMINALPP_SANITIZE"] = self.options.sanitize
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        self.copy("*.hpp", dst="include", src="include")
+        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.so.*", dst="lib", keep_path=False)
+        self.copy("*.dylib", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["terminalpp"]
+
