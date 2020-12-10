12cf7a1cb5d2c5aa85513b59be5f6ebaab1ecf09
Matthew Chaplain <KazDragon@users.noreply.github.com>
2020-12-10 11:39:10 +0100

    Enable Terminal++ to be built with MSVC (#245)
    * Terminal++ can now be built on MSVC.
    
    This involved a couple of changes:
    * Unicode string literals are now explicitly UTF-8 strings.
    * The CMakeLists.txt file restricts some gnu-specific flags to relevant compilers.
    * The type of a constant array was changed to match what was expected.  This removed a couple of warnings.
    * Added an Appveyor script to check that Terminal++ works on MSVC for all future changes.

--- test/glyph_test.cpp
+++ test/glyph_test.cpp
@@ -6,12 +6,12 @@ using testing::ValuesIn;
 
 TEST(glyph_test, comparing_alike_unicode_glyphs_returns_true)
 {
-    ASSERT_EQ(terminalpp::glyph("\U00002501"), terminalpp::glyph("\U00002501"));
+    ASSERT_EQ(terminalpp::glyph(u8"\U00002501"), terminalpp::glyph(u8"\U00002501"));
 }
 
 TEST(glyph_test, comparing_dissimmilar_unicode_glyphs_returns_false)
 {
-    ASSERT_NE(terminalpp::glyph("\U00002501"), terminalpp::glyph("\U00002502"));
+    ASSERT_NE(terminalpp::glyph(u8"\U00002501"), terminalpp::glyph(u8"\U00002502"));
 }
 
 using glyph_string = std::tuple<
@@ -54,20 +54,20 @@ static glyph_string const glyph_strings[] = {
     glyph_string { { 'Z',    terminalpp::ansi::charset::uk    }, "en_uk:Z" },
     glyph_string { { '\x07', terminalpp::ansi::charset::dutch }, "nl:0x07" },
 
-    glyph_string { terminalpp::glyph{"\U00000001"}, "u:0x01" },
-    glyph_string { terminalpp::glyph{"\U00000007"}, "u:0x07" },
-    glyph_string { terminalpp::glyph{"\U00000009"}, "u:\\t"  },
-    glyph_string { terminalpp::glyph{"\U0000000A"}, "u:\\n"  },
-    glyph_string { terminalpp::glyph{"\U0000000D"}, "u:\\r"  },
-    glyph_string { terminalpp::glyph{"\U0000001B"}, "u:0x1B" },
-    glyph_string { terminalpp::glyph{"\U0000005A"}, "u:Z"    },
-    glyph_string { terminalpp::glyph{"\U00000061"}, "u:a"    },
-    glyph_string { terminalpp::glyph{"\U0000007F"}, "u:0x7F" },
-    glyph_string { terminalpp::glyph{"\U00000080"}, "U+0080" },
-    glyph_string { terminalpp::glyph{"\U000007FF"}, "U+07FF" },
-    glyph_string { terminalpp::glyph{"\U00000800"}, "U+0800" },
-    glyph_string { terminalpp::glyph{"\U00002501"}, "U+2501" },
-    glyph_string { terminalpp::glyph{"\U00001701"}, "U+1701" },
+    glyph_string { terminalpp::glyph{u8"\U00000001"}, "u:0x01" },
+    glyph_string { terminalpp::glyph{u8"\U00000007"}, "u:0x07" },
+    glyph_string { terminalpp::glyph{u8"\U00000009"}, "u:\\t"  },
+    glyph_string { terminalpp::glyph{u8"\U0000000A"}, "u:\\n"  },
+    glyph_string { terminalpp::glyph{u8"\U0000000D"}, "u:\\r"  },
+    glyph_string { terminalpp::glyph{u8"\U0000001B"}, "u:0x1B" },
+    glyph_string { terminalpp::glyph{u8"\U0000005A"}, "u:Z"    },
+    glyph_string { terminalpp::glyph{u8"\U00000061"}, "u:a"    },
+    glyph_string { terminalpp::glyph{u8"\U0000007F"}, "u:0x7F" },
+    glyph_string { terminalpp::glyph{u8"\U00000080"}, "U+0080" },
+    glyph_string { terminalpp::glyph{u8"\U000007FF"}, "U+07FF" },
+    glyph_string { terminalpp::glyph{u8"\U00000800"}, "U+0800" },
+    glyph_string { terminalpp::glyph{u8"\U00002501"}, "U+2501" },
+    glyph_string { terminalpp::glyph{u8"\U00001701"}, "U+1701" },
 
 };
 
@@ -81,6 +81,6 @@ TEST(a_glyph, can_be_inserted_into_an_unordered_set)
 {
     std::unordered_set<terminalpp::glyph> g { 
         {},
-        { terminalpp::glyph{"\U000007FF"} }
+        { terminalpp::glyph{u8"\U000007FF"} }
     };
 }
