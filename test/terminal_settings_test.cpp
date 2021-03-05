0d768e68f2f664865051bd84dae4622ca401ccbc
Matthew Chaplain <matthew.chaplain@gmail.com>
2021-03-05 11:43:02 +0100

    Setting the Window Title can use 8-bit codes.

--- test/terminal_settings_test.cpp
+++ test/terminal_settings_test.cpp
@@ -134,6 +134,33 @@ TEST_F(a_terminal_with_support_for_window_title_st, sends_window_title_with_bel)
     expect_sequence("\x1B]2;title\x1B\\"_tb, result_);
 }
 
+namespace {
+
+class a_terminal_with_support_for_window_title_st_and_8bit_control_codes : public a_terminal
+{
+public:
+    a_terminal_with_support_for_window_title_st_and_8bit_control_codes()
+        : a_terminal(
+            []
+            {
+                terminalpp::behaviour beh;
+                beh.supports_window_title_st = true;
+                beh.can_use_eight_bit_control_codes = true;
+                beh.uses_eight_bit_control_codes_by_default = true;
+                return beh;
+            }())
+    {
+    }
+};
+
+}
+
+TEST_F(a_terminal_with_support_for_window_title_st_and_8bit_control_codes, sends_window_title_with_bel)
+{
+    terminal_.write(append_to_result) << terminalpp::set_window_title("title");
+    expect_sequence("\x9D"_tb "2;title\x9C"_tb, result_);
+}
+
 /*
 
 TEST(activating_normal_screen_buffer, sends_use_normal_screen_buffer)
