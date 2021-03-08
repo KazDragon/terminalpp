fcf5c052cdb061f276796808e02ef6e331b75d4a
KazDragon <matthew.chaplain@gmail.com>
2021-03-08 09:06:31 +0100

    Fetch GSL-lite in CI dependencies.

--- .ci/fetch_system_dependencies.sh
+++ .ci/fetch_system_dependencies.sh
@@ -15,6 +15,17 @@ if [ ! -f "$EXTERNAL_ROOT/include/fmt/format.hpp" ]; then
     make -j2 && make install;
 fi
 
+# Install gsl-lite dependency
+if [ ! -f "$EXTERNAL_ROOT/include/gsl/gsl-lite.hpp" ]; then
+    cd "$EXTERNAL_BUILD_ROOT";
+    wget https://github.com/gsl-lite/gsl-lite/archive/v0.34.0.tar.gz;
+    tar -xzf v0.34.0.tar.gz;
+    cd gsl-lite-0.34.0;
+    cmake -DCMAKE_INSTALL_PREFIX="$EXTERNAL_ROOT" -DGSL_LITE_OPT_BUILD_TESTS=Off .;
+    make -j2 && make install;
+fi
+
+# Install googletest dependency
 if [ ! -f "$EXTERNAL_ROOT/include/gtest/gtest.h" ]; then
     cd "$EXTERNAL_BUILD_ROOT";
     wget https://github.com/google/googletest/archive/release-1.10.0.tar.gz;
