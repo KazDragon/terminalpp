08ce82a6318611e55cb771c3b6ea0f8a75ee0586
Matthew Chaplain <KazDragon@users.noreply.github.com>
2021-03-17 14:34:19 +0100

    Cursor position is reset when a terminal changes its size (#267)

--- src/terminal.cpp
+++ src/terminal.cpp
@@ -18,6 +18,12 @@ terminal::terminal(behaviour const &beh)
 void terminal::set_size(extent size)
 {
     state_.terminal_size_ = size;
+
+    // The cursor positions that terminals have after a size change is
+    // inconsistent across implementations.  By resetting our own position
+    // to an unknown one, it ensures that a precise move occurs the next
+    // time the cursor is moved to a position.
+    state_.cursor_position_ = {};
 }
 
 }
