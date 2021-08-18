8efd08864ef8da176ddf42cefd9c3c984c4f7d93
Matthew Chaplain <matthew.chaplain@gmail.com>
2021-08-18 18:45:47 +0200

    Implemented is_control_key.

--- src/virtual_key.cpp
+++ src/virtual_key.cpp
@@ -68,84 +68,6 @@ std::ostream &operator<<(std::ostream &out, vk_modifier const &vkm)
 }
 
 // ==========================================================================
-// OPERATOR<<(VK)
-// ==========================================================================
-std::ostream &operator<<(std::ostream &out, vk const &key)
-{
-    auto is_control_key = [](vk const &control_key)
-    {
-        static constexpr vk control_set_begin = vk::nul;
-        static constexpr vk control_set_end = vk::space;
-
-        return control_key >= control_set_begin && control_key < control_set_end;
-    };
-
-    auto is_abstract_key = [](vk const &abstract_key)
-    {
-        static constexpr vk abstract_key_start = vk::cursor_up;
-
-        return abstract_key >= abstract_key_start;
-    };
-
-    if (is_control_key(key))
-    {
-        switch (key)
-        {
-            case vk::cr :           out << "'\\r'"; break;
-            case vk::lf :           out << "'\\n'"; break;
-            case vk::ht :           out << "'\\t'"; break;
-            default :
-            {
-                boost::io::ios_all_saver ias(out);
-                out << "'\\x" 
-                    << std::hex << std::setfill('0') << std::setw(2) << std::uppercase 
-                    << int(static_cast<unsigned char>(key))
-                    << "'";
-            }
-        }
-    }
-    else if (is_abstract_key(key))
-    {
-        switch (key)
-        {
-            case vk::cursor_up :    out << "cursor_up"; break;
-            case vk::cursor_down :  out << "cursor_down"; break;
-            case vk::cursor_left :  out << "cursor_left"; break;
-            case vk::cursor_right : out << "cursor_right"; break;
-
-            case vk::home :         out << "home"; break;
-            case vk::ins :          out << "ins"; break;
-            case vk::end :          out << "end"; break;
-            case vk::pgup :         out << "pgup"; break;
-            case vk::pgdn :         out << "pgdn"; break;
-
-            case vk::bt :           out << "bt"; break;
-            case vk::enter :        out << "enter"; break;
-
-            case vk::f1 :           out << "f1"; break;
-            case vk::f2 :           out << "f2"; break;
-            case vk::f3 :           out << "f3"; break;
-            case vk::f4 :           out << "f4"; break;
-            case vk::f5 :           out << "f5"; break;
-            case vk::f6 :           out << "f6"; break;
-            case vk::f7 :           out << "f7"; break;
-            case vk::f8 :           out << "f8"; break;
-            case vk::f9 :           out << "f9"; break;
-            case vk::f10 :          out << "f10"; break;
-            case vk::f11 :          out << "f11"; break;
-            case vk::f12 :          out << "f12"; break;
-            default :               out << "unk"; break;
-        }
-    }
-    else
-    {
-        out << "'" << static_cast<char>(key) << "'";
-    }
-
-    return out;
-}
-
-// ==========================================================================
 // OUTPUT_COMMA
 // ==========================================================================
 void output_comma(std::ostream &out, bool &comma)
@@ -230,6 +152,61 @@ std::ostream &operator<<(std::ostream &out, virtual_key const &vk)
     output_repeat_count(out, vk.repeat_count, comma);
     output_input_sequence(out, vk.sequence, comma);
     return out << "]";
+}
+
+// ==========================================================================
+// OPERATOR<<(VK)
+// ==========================================================================
+std::ostream &operator<<(std::ostream &out, vk const &key)
+{
+    if (is_control_key(key))
+    {
+        switch (key)
+        {
+            case vk::cr :           out << "'\\r'"; break;
+            case vk::lf :           out << "'\\n'"; break;
+            case vk::ht :           out << "'\\t'"; break;
+
+            case vk::cursor_up :    out << "cursor_up"; break;
+            case vk::cursor_down :  out << "cursor_down"; break;
+            case vk::cursor_left :  out << "cursor_left"; break;
+            case vk::cursor_right : out << "cursor_right"; break;
+
+            case vk::home :         out << "home"; break;
+            case vk::ins :          out << "ins"; break;
+            case vk::end :          out << "end"; break;
+            case vk::pgup :         out << "pgup"; break;
+            case vk::pgdn :         out << "pgdn"; break;
+
+            case vk::bt :           out << "bt"; break;
+            case vk::enter :        out << "enter"; break;
+
+            case vk::f1 :           out << "f1"; break;
+            case vk::f2 :           out << "f2"; break;
+            case vk::f3 :           out << "f3"; break;
+            case vk::f4 :           out << "f4"; break;
+            case vk::f5 :           out << "f5"; break;
+            case vk::f6 :           out << "f6"; break;
+            case vk::f7 :           out << "f7"; break;
+            case vk::f8 :           out << "f8"; break;
+            case vk::f9 :           out << "f9"; break;
+            case vk::f10 :          out << "f10"; break;
+            case vk::f11 :          out << "f11"; break;
+            case vk::f12 :          out << "f12"; break;
+            default :
+                boost::io::ios_all_saver ias(out);
+                out << "'\\x" 
+                    << std::hex << std::setfill('0') << std::setw(2) << std::uppercase 
+                    << int(static_cast<unsigned char>(key))
+                    << "'";
+        }
+    }
+    else
+    {
+        out << "'" << static_cast<char>(key) << "'";
+    }
+
+    return out;
 }
 
 }
