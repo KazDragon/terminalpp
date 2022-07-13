8457fd9572b7ac3d98ba57a8a9dadbe25086d069
Matthew Chaplain <matthew.chaplain@gmail.com>
2022-07-13 10:57:23 +0200

    Removed imports script to use defaults.

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -14,12 +14,6 @@ class TerminalppTestConan(ConanFile):
         cmake.configure()
         cmake.build()
 
-    def imports(self):
-        self.copy("*.dll", dst="bin", src="bin")
-        self.copy("*.dylib*", dst="bin", src="lib")
-        self.copy('*.so*', dst='bin', src='lib')
-        self.copy('*.a', dst='bin', src='lib')
-
     def test(self):
         if not tools.cross_building(self.settings):
             os.chdir("bin")
