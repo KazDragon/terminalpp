1eff8cf9943460dbebbba752ee698240480958cc
Matthew Chaplain <matthew.chaplain@gmail.com>
2025-06-19 09:30:37 +0000

    refactor: trimmed down mouse_test

--- test/mouse_test.cpp
+++ test/mouse_test.cpp
@@ -18,7 +18,7 @@ TEST(a_default_constructed_mouse_event, has_known_values)
 
 using mouse_event_test_data = std::tuple<
     terminalpp::mouse::event,  // input data
-    std::string                // expected output
+    std::string_view           // expected output
     >;
 
 class mouse_events_with_strings
@@ -28,9 +28,7 @@ class mouse_events_with_strings
 
 TEST_P(mouse_events_with_strings, can_be_streamed_to_an_ostream)
 {
-    auto const &param = GetParam();
-    auto const &rep = std::get<0>(param);
-    auto const &expected_string = std::get<1>(param);
+    auto const &[rep, expected_string] = GetParam();
 
     std::stringstream stream;
     std::ostream &out = stream;
@@ -39,44 +37,38 @@ TEST_P(mouse_events_with_strings, can_be_streamed_to_an_ostream)
     ASSERT_EQ(expected_string, stream.str());
 }
 
-static mouse_event_test_data const mouse_event_strings[] = {
+constexpr mouse_event_test_data const mouse_event_strings[] = {
     // A default report should print out that no button has changed at (0,0)
-    mouse_event_test_data{
-                          terminalpp::mouse::event{},                                              "mouse_event[point(0,0), no-change]"},
+    {terminalpp::mouse::event{},                                             "mouse_event[point(0,0), no-change]"},
 
     // A report with different button states should print out those states
-    mouse_event_test_data{
-                          terminalpp::mouse::event{
-            terminalpp::mouse::event_type::left_button_down},
-                          "mouse_event[point(0,0), lmb]"                                                                               },
-    mouse_event_test_data{
-                          terminalpp::mouse::event{
-            terminalpp::mouse::event_type::middle_button_down},
-                          "mouse_event[point(0,0), mmb]"                                                                               },
-    mouse_event_test_data{
-                          terminalpp::mouse::event{
-            terminalpp::mouse::event_type::right_button_down},
-                          "mouse_event[point(0,0), rmb]"                                                                               },
-    mouse_event_test_data{
-                          terminalpp::mouse::event{terminalpp::mouse::event_type::button_up},
-                          "mouse_event[point(0,0), up]"                                                                                },
-    mouse_event_test_data{
-                          terminalpp::mouse::event{
-            terminalpp::mouse::event_type::no_button_change},
-                          "mouse_event[point(0,0), no-change]"                                                                         },
-    mouse_event_test_data{
-                          terminalpp::mouse::event{
-            terminalpp::mouse::event_type::scrollwheel_down},
-                          "mouse_event[point(0,0), sdn]"                                                                               },
-    mouse_event_test_data{
-                          terminalpp::mouse::event{terminalpp::mouse::event_type::scrollwheel_up},
-                          "mouse_event[point(0,0), sup]"                                                                               },
+    {terminalpp::mouse::event{
+         .action_ = terminalpp::mouse::event_type::left_button_down},
+     "mouse_event[point(0,0), lmb]"                                                                              },
+    {terminalpp::mouse::event{
+         .action_ = terminalpp::mouse::event_type::middle_button_down},
+     "mouse_event[point(0,0), mmb]"                                                                              },
+    {terminalpp::mouse::event{
+         .action_ = terminalpp::mouse::event_type::right_button_down},
+     "mouse_event[point(0,0), rmb]"                                                                              },
+    {terminalpp::mouse::event{
+         .action_ = terminalpp::mouse::event_type::button_up},
+     "mouse_event[point(0,0), up]"                                                                               },
+    {terminalpp::mouse::event{
+         .action_ = terminalpp::mouse::event_type::no_button_change},
+     "mouse_event[point(0,0), no-change]"                                                                        },
+    {terminalpp::mouse::event{
+         .action_ = terminalpp::mouse::event_type::scrollwheel_down},
+     "mouse_event[point(0,0), sdn]"                                                                              },
+    {terminalpp::mouse::event{
+         .action_ = terminalpp::mouse::event_type::scrollwheel_up},
+     "mouse_event[point(0,0), sup]"                                                                              },
 
     // Reports with different co-ordinates should output those values
-    mouse_event_test_data{
-                          terminalpp::mouse::event{
-            terminalpp::mouse::event_type::left_button_down, {15, 17}},
-                          "mouse_event[point(15,17), lmb]"                                                                             },
+    {terminalpp::mouse::event{
+         .action_ = terminalpp::mouse::event_type::left_button_down,
+         .position_ = {15, 17}},
+     "mouse_event[point(15,17), lmb]"                                                                            },
 };
 
 INSTANTIATE_TEST_SUITE_P(
