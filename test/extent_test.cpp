7ce1626b58c8bdf4cab2dff4c005cfe355198227
Matthew Chaplain <matthew.chaplain@gmail.com>
2025-06-19 09:23:50 +0000

    refactor: trimmed down extent_test

--- test/extent_test.cpp
+++ test/extent_test.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include <terminalpp/extent.hpp>
 
+#include <initializer_list>
 #include <sstream>
 #include <tuple>
 
@@ -16,21 +17,19 @@ class extents_with_strings : public testing::TestWithParam<extent_string>
 
 TEST_P(extents_with_strings, can_be_streamed_to_an_ostream)
 {
-    auto const &param = GetParam();
-    auto const &pt = std::get<0>(param);
-    auto const &expected = std::get<1>(param);
+    auto const &[ext, expected] = GetParam();
 
     std::stringstream stream;
     std::ostream &out = stream;
 
-    out << pt;
+    out << ext;
     ASSERT_EQ(expected, stream.str());
 }
 
 INSTANTIATE_TEST_SUITE_P(
     extents_can_be_streamed_to_an_ostream,
     extents_with_strings,
-    ValuesIn(std::vector<extent_string>{
+    ValuesIn(std::initializer_list<extent_string>{
         // clang-format off
         extent_string{ {0, 0},           "extent(0,0)"   },
         extent_string{ {76, 94},         "extent(76,94)" },
