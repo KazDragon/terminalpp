86798218fd0663f3ce91761d4a7406fa6085cd1c
Matthew Chaplain <matthew.chaplain@gmail.com>
2025-06-20 06:48:02 +0000

    refactor: trimmed down virtual key test

--- test/virtual_key_test.cpp
+++ test/virtual_key_test.cpp
@@ -68,7 +68,7 @@ TEST(and_assigning_vk_modifiers, matches_intersection)
 
 using virtual_key_test_data = std::tuple<
     terminalpp::virtual_key,  // input data
-    std::string               // expected output
+    std::string_view          // expected output
     >;
 
 class virtual_keys_with_strings
@@ -78,9 +78,7 @@ class virtual_keys_with_strings
 
 TEST_P(virtual_keys_with_strings, can_be_streamed_to_an_ostream)
 {
-    auto const &param = GetParam();
-    auto const &vk = std::get<0>(param);
-    auto const &expected_string = std::get<1>(param);
+    auto const &[vk, expected_string] = GetParam();
 
     std::stringstream stream;
     std::ostream &out = stream;
@@ -89,147 +87,135 @@ TEST_P(virtual_keys_with_strings, can_be_streamed_to_an_ostream)
     ASSERT_EQ(expected_string, stream.str());
 }
 
-static terminalpp::virtual_key const default_vk = {};
+constexpr terminalpp::virtual_key default_vk = {};
 
-static virtual_key_test_data const virtual_key_strings[] = {
+virtual_key_test_data const virtual_key_strings[] = {
     // A virtual key should just output the wrapper.
-    virtual_key_test_data{terminalpp::virtual_key{},                                               "virtual_key[]"        },
+    {terminalpp::virtual_key{},                                                    "virtual_key[]"     },
     // Virtual keys with non-default printable characters should print those out
-    virtual_key_test_data{
-                          terminalpp::virtual_key{terminalpp::vk::question_mark},
-                          "virtual_key[vk:'?']"                                                                           },
-    virtual_key_test_data{
-                          terminalpp::virtual_key{terminalpp::vk::uppercase_m},
-                          "virtual_key[vk:'M']"                                                                           },
-    virtual_key_test_data{
-                          terminalpp::virtual_key{terminalpp::vk::space},                          "virtual_key[vk:' ']"  },
+    {terminalpp::virtual_key{.key = terminalpp::vk::question_mark},
+     "virtual_key[vk:'?']"                                                                             },
+    {terminalpp::virtual_key{.key = terminalpp::vk::uppercase_m},
+     "virtual_key[vk:'M']"                                                                             },
+    {terminalpp::virtual_key{.key = terminalpp::vk::space},
+     "virtual_key[vk:' ']"                                                                             },
 
     // Virtual keys with whitespace characters should print out slash codes
-    virtual_key_test_data{
-                          terminalpp::virtual_key{terminalpp::vk::lf},                             "virtual_key[vk:'\\n']"},
-    virtual_key_test_data{
-                          terminalpp::virtual_key{terminalpp::vk::cr},                             "virtual_key[vk:'\\r']"},
-    virtual_key_test_data{
-                          terminalpp::virtual_key{terminalpp::vk::ht},                             "virtual_key[vk:'\\t']"},
+    {terminalpp::virtual_key{.key = terminalpp::vk::lf},
+     "virtual_key[vk:'\\n']"                                                                           },
+    {terminalpp::virtual_key{.key = terminalpp::vk::cr},
+     "virtual_key[vk:'\\r']"                                                                           },
+    {terminalpp::virtual_key{.key = terminalpp::vk::ht},
+     "virtual_key[vk:'\\t']"                                                                           },
 
     // Virtual keys with non-printable control characters should print out
     // the hex code of the value.
-    virtual_key_test_data{
-                          terminalpp::virtual_key{terminalpp::vk::esc},
-                          "virtual_key[vk:'\\x1B']"                                                                       },
+    {terminalpp::virtual_key{.key = terminalpp::vk::esc},
+     "virtual_key[vk:'\\x1B']"                                                                         },
 
     // Virtual non-ASCII codes should print the name of the key
-    virtual_key_test_data{
-                          terminalpp::virtual_key{terminalpp::vk::cursor_up},
-                          "virtual_key[vk:cursor_up]"                                                                     },
-    virtual_key_test_data{
-                          terminalpp::virtual_key{terminalpp::vk::cursor_down},
-                          "virtual_key[vk:cursor_down]"                                                                   },
-    virtual_key_test_data{
-                          terminalpp::virtual_key{terminalpp::vk::cursor_left},
-                          "virtual_key[vk:cursor_left]"                                                                   },
-    virtual_key_test_data{
-                          terminalpp::virtual_key{terminalpp::vk::cursor_right},
-                          "virtual_key[vk:cursor_right]"                                                                  },
-    virtual_key_test_data{
-                          terminalpp::virtual_key{terminalpp::vk::home},                           "virtual_key[vk:home]" },
-    virtual_key_test_data{
-                          terminalpp::virtual_key{terminalpp::vk::end},                            "virtual_key[vk:end]"  },
-    virtual_key_test_data{
-                          terminalpp::virtual_key{terminalpp::vk::ins},                            "virtual_key[vk:ins]"  },
-    virtual_key_test_data{
-                          terminalpp::virtual_key{terminalpp::vk::end},                            "virtual_key[vk:end]"  },
-    virtual_key_test_data{
-                          terminalpp::virtual_key{terminalpp::vk::pgup},                           "virtual_key[vk:pgup]" },
-    virtual_key_test_data{
-                          terminalpp::virtual_key{terminalpp::vk::pgdn},                           "virtual_key[vk:pgdn]" },
-    virtual_key_test_data{
-                          terminalpp::virtual_key{terminalpp::vk::bt},                             "virtual_key[vk:bt]"   },
-    virtual_key_test_data{
-                          terminalpp::virtual_key{terminalpp::vk::enter},
-                          "virtual_key[vk:enter]"                                                                         },
-    virtual_key_test_data{
-                          terminalpp::virtual_key{terminalpp::vk::f1},                             "virtual_key[vk:f1]"   },
-    virtual_key_test_data{
-                          terminalpp::virtual_key{terminalpp::vk::f2},                             "virtual_key[vk:f2]"   },
-    virtual_key_test_data{
-                          terminalpp::virtual_key{terminalpp::vk::f3},                             "virtual_key[vk:f3]"   },
-    virtual_key_test_data{
-                          terminalpp::virtual_key{terminalpp::vk::f4},                             "virtual_key[vk:f4]"   },
-    virtual_key_test_data{
-                          terminalpp::virtual_key{terminalpp::vk::f5},                             "virtual_key[vk:f5]"   },
-    virtual_key_test_data{
-                          terminalpp::virtual_key{terminalpp::vk::f6},                             "virtual_key[vk:f6]"   },
-    virtual_key_test_data{
-                          terminalpp::virtual_key{terminalpp::vk::f7},                             "virtual_key[vk:f7]"   },
-    virtual_key_test_data{
-                          terminalpp::virtual_key{terminalpp::vk::f8},                             "virtual_key[vk:f8]"   },
-    virtual_key_test_data{
-                          terminalpp::virtual_key{terminalpp::vk::f9},                             "virtual_key[vk:f9]"   },
-    virtual_key_test_data{
-                          terminalpp::virtual_key{terminalpp::vk::f10},                            "virtual_key[vk:f10]"  },
-    virtual_key_test_data{
-                          terminalpp::virtual_key{terminalpp::vk::f11},                            "virtual_key[vk:f11]"  },
-    virtual_key_test_data{
-                          terminalpp::virtual_key{terminalpp::vk::f12},                            "virtual_key[vk:f12]"  },
+    {terminalpp::virtual_key{.key = terminalpp::vk::cursor_up},
+     "virtual_key[vk:cursor_up]"                                                                       },
+    {terminalpp::virtual_key{.key = terminalpp::vk::cursor_down},
+     "virtual_key[vk:cursor_down]"                                                                     },
+    {terminalpp::virtual_key{.key = terminalpp::vk::cursor_left},
+     "virtual_key[vk:cursor_left]"                                                                     },
+    {terminalpp::virtual_key{.key = terminalpp::vk::cursor_right},
+     "virtual_key[vk:cursor_right]"                                                                    },
+    {terminalpp::virtual_key{.key = terminalpp::vk::home},
+     "virtual_key[vk:home]"                                                                            },
+    {terminalpp::virtual_key{.key = terminalpp::vk::end},
+     "virtual_key[vk:end]"                                                                             },
+    {terminalpp::virtual_key{.key = terminalpp::vk::ins},
+     "virtual_key[vk:ins]"                                                                             },
+    {terminalpp::virtual_key{.key = terminalpp::vk::end},
+     "virtual_key[vk:end]"                                                                             },
+    {terminalpp::virtual_key{.key = terminalpp::vk::pgup},
+     "virtual_key[vk:pgup]"                                                                            },
+    {terminalpp::virtual_key{.key = terminalpp::vk::pgdn},
+     "virtual_key[vk:pgdn]"                                                                            },
+    {terminalpp::virtual_key{.key = terminalpp::vk::bt},                           "virtual_key[vk:bt]"},
+    {terminalpp::virtual_key{.key = terminalpp::vk::enter},
+     "virtual_key[vk:enter]"                                                                           },
+    {terminalpp::virtual_key{.key = terminalpp::vk::f1},                           "virtual_key[vk:f1]"},
+    {terminalpp::virtual_key{.key = terminalpp::vk::f2},                           "virtual_key[vk:f2]"},
+    {terminalpp::virtual_key{.key = terminalpp::vk::f3},                           "virtual_key[vk:f3]"},
+    {terminalpp::virtual_key{.key = terminalpp::vk::f4},                           "virtual_key[vk:f4]"},
+    {terminalpp::virtual_key{.key = terminalpp::vk::f5},                           "virtual_key[vk:f5]"},
+    {terminalpp::virtual_key{.key = terminalpp::vk::f6},                           "virtual_key[vk:f6]"},
+    {terminalpp::virtual_key{.key = terminalpp::vk::f7},                           "virtual_key[vk:f7]"},
+    {terminalpp::virtual_key{.key = terminalpp::vk::f8},                           "virtual_key[vk:f8]"},
+    {terminalpp::virtual_key{.key = terminalpp::vk::f9},                           "virtual_key[vk:f9]"},
+    {terminalpp::virtual_key{.key = terminalpp::vk::f10},
+     "virtual_key[vk:f10]"                                                                             },
+    {terminalpp::virtual_key{.key = terminalpp::vk::f11},
+     "virtual_key[vk:f11]"                                                                             },
+    {terminalpp::virtual_key{.key = terminalpp::vk::f12},
+     "virtual_key[vk:f12]"                                                                             },
     // etc.
 
     // Virtual keys with non-default modifiers should print out the modifier
-    virtual_key_test_data{
-                          terminalpp::virtual_key{default_vk.key, terminalpp::vk_modifier::shift},
-                          "virtual_key[shift]"                                                                            },
-    virtual_key_test_data{
-                          terminalpp::virtual_key{default_vk.key, terminalpp::vk_modifier::ctrl},
-                          "virtual_key[ctrl]"                                                                             },
-    virtual_key_test_data{
-                          terminalpp::virtual_key{default_vk.key, terminalpp::vk_modifier::alt},
-                          "virtual_key[alt]"                                                                              },
-    virtual_key_test_data{
-                          terminalpp::virtual_key{default_vk.key, terminalpp::vk_modifier::meta},
-                          "virtual_key[meta]"                                                                             },
+    {terminalpp::virtual_key{
+         .key = default_vk.key, .modifiers = terminalpp::vk_modifier::shift},
+     "virtual_key[shift]"                                                                              },
+    {terminalpp::virtual_key{
+         .key = default_vk.key, .modifiers = terminalpp::vk_modifier::ctrl},
+     "virtual_key[ctrl]"                                                                               },
+    {terminalpp::virtual_key{
+         .key = default_vk.key, .modifiers = terminalpp::vk_modifier::alt},
+     "virtual_key[alt]"                                                                                },
+    {terminalpp::virtual_key{
+         .key = default_vk.key, .modifiers = terminalpp::vk_modifier::meta},
+     "virtual_key[meta]"                                                                               },
 
-    virtual_key_test_data{
-                          terminalpp::virtual_key{
-            default_vk.key,
-            terminalpp::vk_modifier::shift | terminalpp::vk_modifier::ctrl
-                | terminalpp::vk_modifier::alt | terminalpp::vk_modifier::meta},
-                          "virtual_key[shift|ctrl|alt|meta]"                                                              },
+    {terminalpp::virtual_key{
+         .key = default_vk.key,
+         .modifiers =
+             terminalpp::vk_modifier::shift | terminalpp::vk_modifier::ctrl
+             | terminalpp::vk_modifier::alt | terminalpp::vk_modifier::meta},
+     "virtual_key[shift|ctrl|alt|meta]"                                                                },
 
     // If the repeat count is non-zero, that should be printed out
-    virtual_key_test_data{
-                          terminalpp::virtual_key{default_vk.key, default_vk.modifiers, 1},
-                          "virtual_key[repeat:1]"                                                                         },
-    virtual_key_test_data{
-                          terminalpp::virtual_key{default_vk.key, default_vk.modifiers, 23},
-                          "virtual_key[repeat:23]"                                                                        },
+    {terminalpp::virtual_key{
+         .key = default_vk.key,
+         .modifiers = default_vk.modifiers,
+         .repeat_count = 1},
+     "virtual_key[repeat:1]"                                                                           },
+    {terminalpp::virtual_key{
+         .key = default_vk.key,
+         .modifiers = default_vk.modifiers,
+         .repeat_count = 23},
+     "virtual_key[repeat:23]"                                                                          },
 
     // If the received data is a character, that should be printed out
-    virtual_key_test_data{
-                          terminalpp::virtual_key{
-            default_vk.key,
-            default_vk.modifiers,
-            default_vk.repeat_count,
-            'Z'_tb},
-                          "virtual_key[seq:Z]"                                                                            },
+    {terminalpp::virtual_key{
+         .key = default_vk.key,
+         .modifiers = default_vk.modifiers,
+         .repeat_count = default_vk.repeat_count,
+         .sequence = 'Z'_tb},
+     "virtual_key[seq:Z]"                                                                              },
 
     // If the received data is a sequence, that should be printed out
-    virtual_key_test_data{
-                          terminalpp::virtual_key{
-            default_vk.key,
-            default_vk.modifiers,
-            default_vk.repeat_count,
-            terminalpp::control_sequence{'[', 'H', false, {{"23"_tb}}}},
-                          "virtual_key[seq:control_sequence[initiator:'[', command:'H', "
-        "args:\"23\"]]"                                                                                                   },
+    {terminalpp::virtual_key{
+         .key = default_vk.key,
+         .modifiers = default_vk.modifiers,
+         .repeat_count = default_vk.repeat_count,
+         .sequence =
+             terminalpp::control_sequence{
+                 .initiator = '[',
+                 .command = 'H',
+                 .meta = false,
+                 .arguments = {{"23"_tb}}}},
+     "virtual_key[seq:control_sequence[initiator:'[', command:'H', "
+     "args:\"23\"]]"                                                                                   },
 
     // If we receive several fields, they should be separated with commas
-    virtual_key_test_data{
-                          terminalpp::virtual_key{
-            terminalpp::vk::uppercase_a,
-            terminalpp::vk_modifier::shift,
-            17,
-            'A'_tb},
-                          "virtual_key[vk:'A', shift, repeat:17, seq:A]"                                                  },
+    {terminalpp::virtual_key{
+         .key = terminalpp::vk::uppercase_a,
+         .modifiers = terminalpp::vk_modifier::shift,
+         .repeat_count = 17,
+         .sequence = 'A'_tb},
+     "virtual_key[vk:'A', shift, repeat:17, seq:A]"                                                    },
 };
 
 INSTANTIATE_TEST_SUITE_P(
